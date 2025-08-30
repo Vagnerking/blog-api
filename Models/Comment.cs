0f1fba3e6661834d1ae9a9ebdf7420d5d85f7a87
Vagner Araújo <gnerprocs@gmail.com>
2025-08-30 13:08:37 -0300

    adicionando models

---
+++ Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace blog_api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string AuthorName { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; } = null!;
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,13 @@
+﻿namespace blog_api.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string AuthorName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public ICollection<object> Comment { get; set; } = new List<object>();
+    }
+}
